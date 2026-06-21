da2c31deeda0ede85e18e1029920758b3d7c017b
vergarden <asdfghjkl2008311@163.com>
2026-06-21 16:41:03 +0800

    fix cloudflare build config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,11 @@
-import { defineConfig } from 'vite'
+﻿import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
+  build: {
+    outDir: '../dist',
+  },
   server: {
     port: 5173,
     proxy: {
